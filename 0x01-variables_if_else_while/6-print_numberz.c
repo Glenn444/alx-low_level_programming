8525269091310c2c5d45e2a2a6051d82413e5bdd
Glenn444 <gmark1586@gmail.com>
2022-12-09 06:25:50 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+	for (a = 0; a < 10; a++)
+	{
+		putchar((a % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
+
