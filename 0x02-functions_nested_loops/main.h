65c5fa4629d329be3c39433e83d97e7fa212bd83
Glenn444 <gmark1586@gmail.com>
2022-12-14 14:44:33 +0000

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,13 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
+
 int _putchar(char);
 void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+
 #endif
