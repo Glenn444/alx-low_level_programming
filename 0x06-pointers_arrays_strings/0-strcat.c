3abe72d8e97ea3e1a5285e6b4f317f0fbe8bb27a
Glenn444 <gmark1586@gmail.com>
2022-12-22 01:03:52 -0800

    String cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,10 @@ char *_strcat(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
 
-	while(dest[index++])
+	while (dest[index++])
 		dest_len++;
 
-	for(index = 0; src[index]; index++)
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
