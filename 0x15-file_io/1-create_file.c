ebf125fd8ec79a6b87f4cf319d6d8a89c22920fa
Glenn14449 <gmark1586@gmail.com>
2023-02-10 18:46:29 +0000

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
   * create_file - ...
@@ -19,24 +20,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content)
-		write(fd, text_content, _strlen(text_content));
+		write(fd, text_content, strlen(text_content));
 
 	close(fd);
 	return (1);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -21,26 +22,10 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		if (write(fd, text_content, _strlen(text_content)) == -1)
+		if (write(fd, text_content, strlen(text_content)) == -1)
 			return (-1);
 	}
 
 	close(fd);
 	return (1);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
 }
