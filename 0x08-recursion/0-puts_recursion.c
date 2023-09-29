5ede16539db9c7a6eebc9bd3196af82c975494f8
Glenn14449 <gmark1586@gmail.com>
2023-09-29 12:44:16 +0000

    Program to print a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,13 @@
 *
 * Return: Nothing
 */
-void _puts_recursion(char *s){
-    if(*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-    _putchar(*s);
-    _puts_recursion(s + 1);
-    }
+void _puts_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
