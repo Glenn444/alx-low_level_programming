ecd248e8083d720d6748976e7286a87e843d2453
Glenn444 <gmark1586@gmail.com>
2023-10-20 04:59:59 +0000

    2. Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 			return (NULL);
 
 		temp->str = strdup(str);
-		temp->len = _strlen(str);
+		temp->len = strlen(str);
 		temp->next = *head;
 
 		*head = temp;
@@ -31,21 +31,3 @@ list_t *add_node(list_t **head, const char *str)
 	return (0);
 }
 
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (*s)
-	{
-		s++;
-		c++;
-	}
-
-	return (c);
-}
