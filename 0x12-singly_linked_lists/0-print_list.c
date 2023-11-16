78358e80809b46135f66a5b22d1f20f820fd2f17
Glenn444 <gmark1586@gmail.com>
2023-11-16 15:37:35 +0000

    Change to 0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,13 +13,13 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h->str != NULL)
+		if (h->str == NULL)
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		count++;
 		h = h->next;
