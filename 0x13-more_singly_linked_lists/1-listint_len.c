02dd206eef86b3f95e5e93a437c770e307b0de74
Glenn444 <gmark1586@gmail.com>
2023-11-20 22:43:38 +0000

    1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* listint_len - number of element in a list
+* @h: pointer to the head node
+* Return - the number of elements in a list
+**/
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while(h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
