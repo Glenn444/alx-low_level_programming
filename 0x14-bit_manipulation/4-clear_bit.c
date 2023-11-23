d277a50eb9fe4323409b79b75e9a6742b2614748
Glenn444 <gmark1586@gmail.com>
2023-11-23 21:21:21 +0000

    Sets value of a bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * clear_bit - clear bit
+ * @n: integer
+ * @index: index starting from 0 of the bit you want to set
+ * Return: 1 if success, -1 if error
+**/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8) 
+	{
+		return (-1);
+	}
+
+	mask = 1UL << index;
+	*n &= ~mask;
+
+	return (1);
+}
